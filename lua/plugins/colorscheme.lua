baaf8c18f6ea476899460d884b30bbd1cfa62073
Seb Smith <seb-smith@live.com>
2025-01-07 16:33:12 +0000

    Update theme

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,16 @@
+return {
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin-mocha",
+    },
+  },
+}
+-- Uncomment below for tokyonight theme
+-- return {
+--   "folke/tokyonight.nvim",
+--   lazy = true,
+--   opts = { style = "night" },
+-- }
