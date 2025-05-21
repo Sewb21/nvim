cd5015159074270989362c47cd93a7fb2d729dfb
Seb Smith <seb@sebsmith.dev>
2025-05-21 18:20:04 +0100

    Remove rubocop from nvim config

--- lua/plugins/default.lua
+++ lua/plugins/default.lua
@@ -100,12 +100,6 @@ return {
         solargraph = {
           enabled = lsp == "solargraph",
         },
-        rubocop = {
-          -- If Solargraph and Rubocop are both enabled as an LSP,
-          -- diagnostics will be duplicated because Solargraph
-          -- already calls Rubocop if it is installed
-          enabled = formatter == "rubocop" and lsp ~= "solargraph",
-        },
         standardrb = {
           enabled = formatter == "standardrb",
         },
