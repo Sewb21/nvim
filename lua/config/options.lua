2835b1b629d6d730dedb95b86f080ee124d16f3d
Seb Smith <seb@sebsmith.dev>
2026-05-04 10:10:26 +0100

    Add biome

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,6 +2,7 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.g.lazyvim_ruby_lsp = "ruby_lsp"
+vim.g.lazyvim_prettier_needs_config = true
 vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
